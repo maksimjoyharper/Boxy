16341d6c7f9c650c1d6fa7430326e462f95a3407
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-12-05 17:21:42 +0300

    updated navigation panel

---
+++ src/components/homeNavigation/homeTickets.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import style from "./homeNavigation.module.scss";
+
+interface IHome {
+    user: any
+}
+
+function wordModificator(number : number){
+    if (number === 1) {
+      return `${number} билет`;
+    } else if (number >= 2 && number < 5) {
+      return `${number} билета`;
+    } else {
+      return `${number} билетов`;
+    }
+  }
+
+export const HomeTickets = ({user}: IHome) => {
+    const [selectedTicket, setSelectedTicket] = useState("regular");
+
+    return (
+        <>
+        <label
+          className={`${
+            selectedTicket === "regular"
+              ? `${style.label__tickets} ${style.active}`
+              : style.label__tickets
+          }`}
+          htmlFor="tickets"
+        >
+          <span>{user && wordModificator(user?.tickets)}</span>
+          <input
+            checked={selectedTicket === "regular"}
+            onChange={() => setSelectedTicket("regular")}
+            id="tickets"
+            name="tickets"
+            type="radio"
+          />
+        </label>
+        <label
+          className={`${
+            selectedTicket === "premium"
+              ? `${style.label__tickets} ${style.active}`
+              : style.label__tickets
+          }`}
+          htmlFor="premium-tickets"
+        >
+          <span>{user && wordModificator(user?.premium_tickets)}</span>
+          <input
+            checked={selectedTicket === "premium"}
+            onChange={() => setSelectedTicket("premium")}
+            name="tickets"
+            type="radio"
+          />
+        </label>
+        </>
+    )
+}
