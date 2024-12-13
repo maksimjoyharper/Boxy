1e9d305260f0d8744af251e3ca7a69cf20702575
maksimjoyharper <makksss1123@icloud.com>
2024-12-13 07:57:12 +0300

    feat: icon catalog

--- src/components/catalogItem/catalogItem.tsx
+++ src/components/catalogItem/catalogItem.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import style from "./catalogItem.module.scss";
+import { getImgCatalog } from "../../features/getImgCatalog";
 
 interface ICatalog {
   id: string;
@@ -10,6 +12,11 @@ interface ICatalog {
 }
 
 export const CatalogItem = ({ id, name, price, description }: ICatalog) => {
+  const [icon, setIcon] = useState<string>();
+  useEffect(() => {
+    getImgCatalog(name, setIcon);
+  }, [name]);
+
   return (
     <li key={id} className={style.catalog__item}>
       <div className={style.catalog__upper}>
@@ -17,7 +24,7 @@ export const CatalogItem = ({ id, name, price, description }: ICatalog) => {
         <h2 className={style.catalog__info}>{name}</h2>
       </div>
       <div className={style.catalog__down}>
-        <img className={style.catalog__img} src="" alt="предмет каталога" />
+        <img className={style.catalog__img} src={icon} alt="предмет каталога" />
         <span className={style.catalog__profession}>{description}</span>
       </div>
     </li>
---
+++ src/features/getImgCatalog.ts
@@ -0,0 +1,38 @@
+import iconBook from "../assets/png/catalog/iconBook.png";
+import iconSale5 from "../assets/png/catalog/sale5.png";
+import iconSale10 from "../assets/png/catalog/sale10.png";
+import iconUsefulM from "../assets/png/catalog/useful_materials.png";
+import iconMiniCurs from "../assets/png/catalog/miniCurs.png";
+import iconMerch from "../assets/png/catalog/iconMerch.png";
+
+export const getImgCatalog = (
+  name: string,
+  setIcon: (icon: string) => void
+) => {
+  switch (name) {
+    case "Пройдите профтест на ....":
+      return setIcon(iconBook);
+    case "Скидка 5% на все курсы":
+      return setIcon(iconSale5);
+    case "Скидка 10% на все курсы":
+      return setIcon(iconSale10);
+    case "Python-разработчик":
+      return setIcon(iconUsefulM);
+    case "Бизнес-аналитик":
+      return setIcon(iconUsefulM);
+    case "Веб-дизайнер":
+      return setIcon(iconUsefulM);
+    case "40 правил идеального текста для соцсетей":
+      return setIcon(iconUsefulM);
+    case "Как найти клиентов фрилансеру":
+      return setIcon(iconUsefulM);
+    case "Введение в программирование":
+      return setIcon(iconMiniCurs);
+    case "Введение в маркетинг":
+      return setIcon(iconMiniCurs);
+    case "Введение в дизайн":
+      return setIcon(iconMiniCurs);
+    case "Эксклюзивная байка":
+      return setIcon(iconMerch);
+  }
+};
