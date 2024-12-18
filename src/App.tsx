19c10d8f1e61f36e6bf41113d1c70f3f6f24d4ab
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-12-19 01:19:08 +0300

    upd styles and screen height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { lazy, Suspense } from "react";
-import "./App.css";
+import "./styles/App.css";
 import { Route, Routes } from "react-router-dom";
 import { useTelegram } from "./hooks/telegram/telegram";
 
 const Layout = lazy(() => import("./pages/Layout/Layout"));
 const Game = lazy(() => import("./pages/Game/Game"));
 const Home = lazy(() => import("./pages/Home/Home"));
-const Leaderboard = lazy(() => import("./pages/LeaderBoard/LeaderBoard"));
+const Leaderboard = lazy(() => import("./pages/Leaderboard/LeaderBoard"));
 const Catalog = lazy(() => import("./pages/Catalog/Catalog"));
 const Friends = lazy(() => import("./pages/Friends/Friends"));
 const Tasks = lazy(() => import("./pages/Tasks/Tasks"));
